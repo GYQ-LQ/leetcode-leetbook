3553071d6139dd9be00f7e1fec27095576b3b464
龚岳卿 <1727728017@qq.com>
2020-09-14 10:50:26 +0800

    week

---
+++ weekly-contest/206/2.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: Quinn
+ * @Date: 2020-09-13 10:32:25
+ * @LastEditTime: 2020-09-13 10:32:43
+ * @LastEditors: quinn
+ * @Description:  
+ */
+/**
+ * @param {number} n
+ * @param {number[][]} preferences
+ * @param {number[][]} pairs
+ * @return {number}
+ */
+var unhappyFriends = function (n, preferences, pairs) {
+
+};
